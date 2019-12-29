d2a758c17f3f7e4c599796ba9247126e2ce66095
Shashwat Gupta <shashwatgupta1998@gmail.com>
2019-12-30 02:07:35 +0530

    fix image size

--- newspaper.py
+++ newspaper.py
@@ -64,16 +64,18 @@ class NewspaperConfig(Config):
 
     # We use a GPU with 12GB memory, which can fit two images.
     # Adjust down if you use a smaller GPU.
-    IMAGES_PER_GPU = 2
+    IMAGES_PER_GPU = 8
 
     # Number of classes (including background)
-    NUM_CLASSES = 1 + 1  # Background + balloon
+    NUM_CLASSES = 1 + 1  # Background + newspaper
 
     # Number of training steps per epoch
     STEPS_PER_EPOCH = 100
 
     # Skip detections with < 90% confidence
     DETECTION_MIN_CONFIDENCE = 0.9
+
+    IMAGE_MAX_DIM = 256
 
 
 ############################################################
